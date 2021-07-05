0cd40003f287126b3d05c83bf014487c7a67986e
Paloma Cortes <33705849+palomagabic@users.noreply.github.com>
2021-07-04 21:15:45 -0400

    Update settings.py

--- umami/settings.py
+++ umami/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'umami.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'umami',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
